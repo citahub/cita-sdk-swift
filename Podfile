67f3df2fc828e33d53561d9da366549c2d2b51ee
Yate Fulham <yate@cryptape.com>
2018-08-09 12:41:24 +0900

    Create NervosSwift framework project

---
+++ Podfile
@@ -0,0 +1,13 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '9.0'
+
+target 'Nervos' do
+  use_frameworks!
+  use_modular_headers!
+
+  pod 'web3swift', git: 'https://github.com/BANKEX/web3swift', tag: '1.1.1'
+
+  target 'NervosTests' do
+    inherit! :search_paths
+  end
+end
---
+++ Source/Nervos.h
@@ -0,0 +1,19 @@
+//
+//  Nervos.h
+//  Nervos
+//
+//  Created by Yate Fulham on 2018/08/09.
+//  Copyright © 2018 Cryptape. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Nervos.
+FOUNDATION_EXPORT double NervosVersionNumber;
+
+//! Project version string for Nervos.
+FOUNDATION_EXPORT const unsigned char NervosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Nervos/PublicHeader.h>
+
+
