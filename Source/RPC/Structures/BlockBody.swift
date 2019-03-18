7b21414c4333d26ebdd75d8142fddf16eecabe16
LuFP <lfp@cryptape.com>
2019-03-18 19:43:34 +0800

    feat: add from to body for BlockBody

--- Source/RPC/Structures/BlockBody.swift
+++ Source/RPC/Structures/BlockBody.swift
@@ -10,13 +10,17 @@ import Foundation
 
 public struct BlockBody: Decodable {
     public var transactions: [BlockTransaction]? = []
+    public var from: String = ""
     enum CodingKeys: String, CodingKey {
         case transactions
+        case from
     }
 
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         let transactions = try container.decode([BlockTransaction]?.self, forKey: .transactions)
         self.transactions = transactions
+        let from = try container.decode(String.self, forKey: .from)
+        self.from = from
     }
 }
